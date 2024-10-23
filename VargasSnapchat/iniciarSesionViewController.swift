360f66891bd891cf66ef36388cb1923415fc4884
MindOverMatterl <albert.vargas@tecsup.edu.pe>
2024-10-23 18:36:43 -0500

    nuevo

--- VargasSnapchat/iniciarSesionViewController.swift
+++ VargasSnapchat/iniciarSesionViewController.swift
@@ -1,36 +1,79 @@
-//
-//  ViewController.swift
-//  VargasSnapchat
-//
-//  Created by Albert Vargas on 16/10/24.
-//
-
 import UIKit
 import FirebaseAuth
-import Darwin
-
+import Firebase
+import GoogleSignIn
 
 class iniciarSesionViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    
+
+    @IBOutlet weak var signInButton: GIDSignInButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureGoogleSignIn()
     }
+    
+   
     @IBAction func iniciarSesionTapped(_ sender: Any) {
-        Auth.auth().signIn(withEmail: emailTextField.text!,password: passwordTextField.text!) {(user, error) in print("Intentando iniciar sesion")
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            print("Intentando iniciar sesión")
             if error != nil {
-                print("Se presento el siguiente error: \(String(describing: error) )")
-                
-            }else{
-                print("Inicio de sesion exitoso")
+                print("Se presentó el siguiente error: \(String(describing: error))")
+                Auth.auth().createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!, completion: {(user, error) in
+                    print("Intentando crear un usuario")
+                    if error != nil{
+                        print("Se presento el siguiente error al crear el usuario: \(error)")
+                    }else{
+                        print("El usuario fue creado exitosamente")
+                    }
+                })
+            } else {
+                print("Inicio de sesión exitoso")
             }
         }
-        
     }
+    
+
+    func configureGoogleSignIn() {
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
 
 
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+    }
+    
+    
+    @IBAction func botongoogle(_ sender: Any) {
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { [unowned self] result, error in
+            guard error == nil else {
+                print("Error al iniciar sesión con Google: \(String(describing: error))")
+                return
+            }
+
+            guard let user = result?.user,
+                  let idToken = user.idToken?.tokenString else {
+                print("Error al obtener el ID Token de Google")
+                return
+            }
+
+            
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                           accessToken: user.accessToken.tokenString)
+
+  
+            Auth.auth().signIn(with: credential) { authResult, error in
+                if let error = error {
+                    print("Error al autenticar con Firebase: \(error.localizedDescription)")
+                    return
+                }
+
+                print("Inicio de sesión con Google exitoso")
+            }
+        }
+    }
+    
+   
+   
 }
-
